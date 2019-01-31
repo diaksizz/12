cdbc72fbb26de63003d32f6546f40527d928f339
diaksizz <adityapratama2012@gmail.com>
2019-01-31 17:36:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,21 +3,21 @@
 
 
 // user_name boleh kosong //
-$user_name="Okta Aditya Pratama";
+$user_name=Okta Aditya Pratama;
 
 //user_gaid wajib isi
-$user_gaid= "7814a083-8525-475a-9387-bc9ce6972319";
+$user_gaid=7814a083-8525-475a-9387-bc9ce6972319;
 //user_token wajib di isi //
-$user_token="AbxYgKaqmcb2A_Kv";
+$user_token=AbxYgKaqmcb2A_Kv;
 
 //user_id wajib di isi //
-$user_id="Facebook_1446933258774393";
+$user_id=Facebook_1446933258774393;
 
 // invite_code wajib di isi //
-$invite_code="F6cs4Qat4b";
+$invite_code=F6cs4Qat4b;
 
 // Bearer wajib di isi //
-$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDQ2OTMzMjU4Nzc0MzkzIiwiZXhwIjoiMTU1MTUxODg5NDU3NSJ9.AiFujEtzM0CSpzqWY1jPr_ILHE8wJQnax9epnzz8V-k ";
+$Bearer=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDQ2OTMzMjU4Nzc0MzkzIiwiZXhwIjoiMTU1MTUxODg5NDU3NSJ9.AiFujEtzM0CSpzqWY1jPr_ILHE8wJQnax9epnzz8V-k;
 
 
 
